58e51bf4617577780c3b56bf3e0db79f91317110
Sneha Sunny <snehasunny321@gmail.com>
2023-02-09 14:24:09 +0530

    Add or Delete card

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,11 @@
 import {useState} from 'react'
 import Card from './shared/Card'
+import PropTypes from 'prop-types'
 
 function FeedbackItem({item}){
 
     return (
-        <Card reverse={true}>
+        <Card>
             <div className="num-display">{item.rating}</div>
             <div className="text-display">{item.text}
             </div>
