6818cdb44827640a84f15b87e6ecbf471e69787b
Sneha Sunny <snehasunny321@gmail.com>
2023-02-09 11:49:40 +0530

    Adding feedback comments using components in React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import {useState} from 'react'
 import React from 'react'
-import FeedbackItem from './components/FeedbackItem'
+import FeedbackList from './components/FeedbackList'
 import Header from './components/Header'
+import FeedbackData from './data/FeedbackData'
 
 function App(){
+    const [feedback, setFeedback] = useState(FeedbackData)
+
     return (
         <>
         <Header />
         <div className="container">
-            <FeedbackItem/>       
+            <FeedbackList feedback={feedback}/>       
         </div>
 
         </>
